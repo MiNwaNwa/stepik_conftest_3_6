8d7c5d6d650535db83beb4c57b809b15722b09a6
mike-novo <test-novo@yandex.ru>
2021-12-13 14:03:53 +0300

    Added assert

--- test_items.py
+++ test_items.py
@@ -4,12 +4,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import  WebDriverWait
 import time
 
-def test_big_decision(browser):
+def test_correct_language_page(browser):
     # Открываем страницу по ссылке
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207//")
 # Визуально убедимся, что язык корректный
-    time.sleep(5)
-    # Находим нужную кнопку по классу и убеждаемся, что она станет кликабельной
-    # (нет смысла ставить assert, так как если кнопка не будет найдена, мы и так получим исключение)
-    WebDriverWait(browser, 15).until(EC.element_to_be_clickable((By.CLASS_NAME, "btn-add-to-basket")))
-    # browser.find_element_by_class_name("btn-add-to-basket")
+    time.sleep(15)
+# Находим нужную кнопку по классу и убеждаемся, что она станет кликабельной
+    button_basket = WebDriverWait(browser, 15).until(EC.element_to_be_clickable((By.CLASS_NAME, "btn-add-to-basket")))
+# Проверим что результат поиска элемента не пустой
+    assert button_basket is not None
